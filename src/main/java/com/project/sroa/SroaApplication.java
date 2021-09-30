1e8d88b6f8b45dbeb6bcfa2963e045cd29151474
jje951122 <76868571+jje951122@users.noreply.github.com>
2021-09-30 16:52:35 +0900

    프로젝트 파일 생성

---
+++ src/main/java/com/project/sroa/SroaApplication.java
@@ -0,0 +1,13 @@
+package com.project.sroa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SroaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SroaApplication.class, args);
+    }
+
+}
