515a3caf65d28a52a451ae061052af59b91a4861
jje951122 <76868571+jje951122@users.noreply.github.com>
2021-10-04 15:23:23 +0900

    jueon 브랜체 생성, writeEvaluation 작업

--- src/main/java/com/project/sroa/controller/EvaluationController.java
+++ src/main/java/com/project/sroa/controller/EvaluationController.java
@@ -2,6 +2,7 @@ package com.project.sroa.controller;
 
 import com.project.sroa.model.Evaluation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,4 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class EvaluationController {
     //애플리케이션과의 통신은 요청이 들어오고 응답을 보내면 연결이 끊기기 떄문에
     //return data를 하면 data를 어플리케이션에 보냄
+    @PostMapping("/user/evaluation/writeEvaluation")
+    public void writeEvaluation(){
+
+    }
 }
